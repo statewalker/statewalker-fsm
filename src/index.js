db231f8f497e1cf3482bcb40edec38d56731a7ed
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2022-09-07 07:16:08 +0200

    feature: add an initialization function for async processes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-export * from "@statewalker/tree";
 export { default as buildStateDescriptor } from "./buildStateDescriptor.js";
 export { default as checkProcessDescriptor } from "./checkProcessDescriptor.js";
 export { default as getStateDescriptor } from "./getStateDescriptor.js";
@@ -6,6 +5,7 @@ export { default as getAllStateKeys } from "./getAllStateKeys.js";
 export { default as getAllTransitions } from "./getAllTransitions.js";
 export { default as getTargetStateKey } from "./getTargetStateKey.js";
 export { default as KEYS } from "./KEYS.js";
-export { default as newAsyncProcess } from "./newAsyncProcess.js";
+export { default as initAsyncProcess } from "./initAsyncProcess.js";
+export { default as getAllTransitions } from "./getAllTransitions.js";
 export { default as newProcessInstance } from "./newProcessInstance.js";
 export { default as newSyncProcess } from "./newSyncProcess.js";
---
+++ src/initAsyncProcess.js
@@ -0,0 +1,62 @@
+import newAsyncProcess from "./newAsyncProcess.js";
+
+export default function initProcess({ config, handler, handleError = console.error }) {
+  const { newAsyncProcess } = FSM;
+  let process;
+  const before = async (process) => {
+    process._started = true;
+    const state = process.current;
+    handler(state);
+    await state.init.run();
+  };
+  const after = async (process) => {
+    const state = process.current;
+    await state.done.run();
+  };
+
+  process = newAsyncProcess({
+    config,
+    before,
+    after,
+    newProcess: (descriptor) => {
+      return {
+        descriptor,
+        config,
+        status: 0,
+        stack: [],
+        event: undefined,
+        current: undefined
+      };
+    },
+    newState: (state) => {
+      state.init = newStateMethod();
+      state.done = newStateMethod();
+      state.dispatch = (...args) => state.process.dispatch(...args);
+      state.getEvent = () => state.process.event || {};
+      state.getEventKey = () => state.getEvent().key;
+      return state;
+    },
+    handleError
+  });
+  process.dispatch = (key, params = {}) => {
+    process.next({ key, params });
+  };
+  return process;
+
+  function newStateMethod() {
+    const list = [];
+    return Object.assign((m) => list.push(m), {
+      run: async (...args) => {
+        // We use the index to iterate over the list: the list can be updated by called methods
+        for (let i = 0; i < list.length; i++) {
+          try {
+            list[i] && (await list[i](...args));
+          } catch (error) {
+            console.error(error);
+            await handleError(error);
+          }
+        }
+      }
+    });
+  }
+}
