277b95495a697ad0e0f2d106e217bfd37baa73aa
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2024-11-11 22:20:35 +0100

    chore: update build configuration: simplify exports

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from "./FsmProcess.ts";
 export * from "./FsmState.ts";
 export * from "./FsmStateConfig.ts";
 export * from "./FsmStateDescriptor.ts";
+export * from "./utils/index.ts";
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,4 @@
-export * from "./handlers.ts";
 export * from "./printer.ts";
 export * from "./process.ts";
 export * from "./tracer.ts";
 export * from "./transitions.ts";
-export * from "./newFsmStateHandler.ts";
