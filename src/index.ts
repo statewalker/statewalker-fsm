de1628e325950e073bf4f20e7c2cd7ad558a66d8
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2025-08-29 00:32:22 +0200

    fix: add missing exports

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ export * from "./FsmState.ts";
 export * from "./FsmStateConfig.ts";
 export * from "./FsmStateDescriptor.ts";
 export * from "./newFsmProcess.ts";
-// export * from "./orchestrator/index.ts";
+export * from "./orchestrator/index.ts";
 export * from "./utils/index.ts";
